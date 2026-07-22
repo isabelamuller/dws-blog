279a09645e9e264743f505291603d5cee360d08d
Isabela Müller <isabelamuller1@hotmail.com>
2026-07-21 21:55:32 -0300

    create ArticleCard component

---
+++ src/components/ArticleCard/index.tsx
@@ -0,0 +1,28 @@
+import styles from "./styles.module.css";
+import { formatDate } from "../../utils/formatDate";
+import { getLastName } from "../../utils/getAuthorLastName";
+import type { IArticleCardProps } from "./types";
+
+export const ArticleCard = ({ data }: IArticleCardProps) => {
+  const { title, author, createdAt, categories, thumbnail_url, content } = data;
+
+  return (
+    <div className={styles.card}>
+      <img src={thumbnail_url} alt="" className={styles.image} />
+      <div className={styles.content}>
+        <div className={styles.topContent}>
+          <span className={styles.date}>{formatDate(createdAt)}</span>{" "}
+          <span className={styles.dot}>•</span>
+          <span>{getLastName(author.name)}</span>
+        </div>
+        <span className={styles.title}>{title}</span>
+        <span className={styles.description}>{content}</span>
+      </div>
+      <div className={styles.categories}>
+        {categories.map((category) => (
+          <span>{category.name}</span>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/components/ArticleCard/types.d.ts
@@ -0,0 +1,5 @@
+import type { IPostProps } from "../../api/types";
+
+export interface IArticleCardProps {
+  data: IPostProps;
+}
