03e429d24c8bbd369c237afe5f198df198b3e6bb
Isabela Müller <isabelamuller1@hotmail.com>
2026-07-22 12:02:53 -0300

    create new util formatSlug

---
+++ src/utils/formatSlug.ts
@@ -0,0 +1,9 @@
+export const formatSlug = (text: string) => {
+  return text
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/[^a-z0-9]+/g, "-")
+    .replace(/^-+|-+$/g, "");
+};
