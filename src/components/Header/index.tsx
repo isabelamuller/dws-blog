6e8ff0836fb2d69ff51f4b306d7392fb9554f380
Isabela Müller <isabelamuller1@hotmail.com>
2026-07-22 14:30:08 -0300

    add search bar to Header and fix some styles

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,13 @@
+import { SearchBar } from "../SearchBar";
 import styles from "./styles.module.css";
 
 export const Header = () => {
   return (
     <nav className={styles.nav}>
       <a href="/" className={styles.logo}>
-        <img width={200} src="/icons/dentsu-logo.svg" alt="" />
+        <img width={200} src="/icons/dentsu-logo.svg" alt="Dentsu logo" />
       </a>
-      <div>search bar</div>
+      <SearchBar />
     </nav>
   );
 };
